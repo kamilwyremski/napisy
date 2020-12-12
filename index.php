97c2f54af8279490d52c3b3ec9301afad4009cf4
Kamil Wyremski <kamil.wyremski@gmail.com>
2020-12-12 07:15:28 -0500

    viewport dla urządzeń mobilnych

--- index.php
+++ index.php
@@ -84,10 +84,12 @@ if(!empty($_FILES['plik']) and isset($_POST['przesuniecie'])){
 	}
 }
 	
-?><!doctype html>
-<html>
-<head>
-	<meta charset="UTF-8" />
+?><!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Przesuwanie napisów</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 </head>
